52f8ca99e4aa23c4ee5a683b5a529cdf0de24b3e
Bharath <bharath.dasaraju@advancegroup.com>
2022-07-02 07:22:43 +0800

    kafka source-systems --> Producers --> brokers --> consumers --> Target Systems

---
+++ 1.Begin_Kafka/demo.sh
@@ -0,0 +1,9 @@
+Data integration between the source system and Target system
+
+Apache Kafka: A distributes streaming platform. Its decoupling of data streams and systems.
+
+ Kafka used for micro-services pub/sub
+
+
+
+
