b4f46ad728a9c815792a27229487aa01c3371857
Bharath <bharath.dasaraju@advancegroup.com>
2022-07-02 12:11:33 +0800

    Kafka topic, partitions and offset

---
+++ 1.Begin_Kafka/1.demo_kafka_topics.sh
@@ -0,0 +1,5 @@
+kafka Topic: a particular stream of data in kafka cluster.
+
+kafka topic can be made up of several partitions
+
+when you write data to partitions it is incremented by a number called incerementa id in turn called "kafka partition offset"
