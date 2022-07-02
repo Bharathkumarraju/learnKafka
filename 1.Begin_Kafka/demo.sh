c2656943b0a16c396d96eb6592cc33911a2019ea
Bharath <bharath.dasaraju@advancegroup.com>
2022-07-02 08:13:59 +0800

    learn kaska producers brokers consumers zookeeper

--- 1.Begin_Kafka/demo.sh
+++ 1.Begin_Kafka/demo.sh
@@ -5,5 +5,27 @@ Apache Kafka: A distributes streaming platform. Its decoupling of data streams a
  Kafka used for micro-services pub/sub
 
 
+kafka craft mode:
+Apache Kafka Raft (KRaft) is the consensus protocol that was introduced to remove Apache Kafkas dependency on ZooKeeper for metadata management.
+
+kafka zookeeper vs Kafka raft mode. i.e. KRaft
+
+Kafka Connect
+kafka Streams
+Confluent Schema Registry
+
+
+kafka topics, partitions, and multi-brokers setup
+
+Writing producers and consumers
+
+1. Kafka Connect API
+2. Kafka Streams API:
+3. ksqlDB: Write Kafka steams applications using SQL
+4. Confluent Components: REST Proxy and Schema Registry
+5. Kafka Security
+6. Kafka monitoring and operations: Use prmoetheus and grafana to monitor kafka
+7. Kafka cluster setup and administration --> how kafka and zookeeper works.
+
 
 
