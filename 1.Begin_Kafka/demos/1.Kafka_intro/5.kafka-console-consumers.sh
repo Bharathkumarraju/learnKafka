52b524d4fcc61b720c5c9a56690fccfa8e3c590e
Bharath <bharath.dasaraju@advancegroup.com>
2022-07-03 18:02:03 +0800

    all about kafka topics

--- 1.Begin_Kafka/demos/1.Kafka_intro/5.kafka-console-consumers.sh
+++ 1.Begin_Kafka/demos/1.Kafka_intro/5.kafka-console-consumers.sh
@@ -44,3 +44,41 @@ CreateTime:1656829956098	null	this is bharath
 bahrathkumaraju@Bahrathkumarajus-MacBook-Pro ~ %
 
 
+
+
+kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first-topic --from-beginning --formatter kafka.tools.DefaultMessageFormatter --property print.timestamp=true --property print.key=true --property print.value=true
+
+
+bahrathkumaraju@Bahrathkumarajus-MacBook-Pro ~ % kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic first-topic --from-beginning --formatter kafka.tools.DefaultMessageFormatter --property print.timestamp=true --property print.key=true --property print.value=true
+CreateTime:1656837264550	null	two
+CreateTime:1656837269566	null	hi
+CreateTime:1656837270468	null	hello
+CreateTime:1656837285013	null	what is diff between python and rust
+CreateTime:1656837288625	null	66
+CreateTime:1656837289556	null	7
+CreateTime:1656837291606	null	19
+CreateTime:1656837293968	null	45
+CreateTime:1656837294520	null	56
+CreateTime:1656837377881	three	 3
+CreateTime:1656837388110	six	 6
+CreateTime:1656837397818	nine	 9
+CreateTime:1656837263140	null	one
+CreateTime:1656837268532	null	four
+CreateTime:1656837274326	null	hw r u
+CreateTime:1656837289862	null	8
+CreateTime:1656837290157	null	9
+CreateTime:1656837374913	two	 2
+CreateTime:1656837384329	five	 5
+CreateTime:1656837265934	null	three
+CreateTime:1656837286711	null	5
+CreateTime:1656837287002	null	6
+CreateTime:1656837292673	null	45
+CreateTime:1656837293147	null	45
+CreateTime:1656837293574	null	45
+CreateTime:1656837371034	one	 1
+CreateTime:1656837380975	four	 4
+CreateTime:1656837391293	seven	 7
+CreateTime:1656837394297	eight	 8
+CreateTime:1656837400715	ten	 10
+
+
