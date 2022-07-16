9ddc885429f38f8d2fa60da69ba2aa10ab4029f4
Bharath <bharath.dasaraju@advancegroup.com>
2022-07-17 07:32:57 +0800

    zookeeper and kafka cluster  setup

---
+++ 2.Kafka_cluster_setup/demos/1.demo.sh
@@ -0,0 +1,25 @@
+# test
+
+zookeeper server --> kafka broker
+
+
+ZooNavigator
+Kafka Manager
+Kafka Topics UI
+Confluent Schema Registry
+Confluent REST Proxy
+
+
+Running kafka and zookeeper in docker containers
+
+1.Zookeeper Quorum Architecture
+2. Kafka cluster Architecture
+
+
+ZooNavigator
+Kafka Manager
+Kafka Topics UI
+Confluent Schema Registry
+Confluent REST Proxy
+
+
